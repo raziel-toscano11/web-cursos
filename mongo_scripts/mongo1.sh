cc8d04a78b17b747ce9ecc692c4ff9dd0cfedbab
raziel-toscano11 <razi.tuf11@gmail.com>
2024-10-18 09:24:35 -0600

    Se agregaron scripts de mongo

---
+++ mongo_scripts/mongo1.sh
@@ -0,0 +1,3 @@
+curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
+   --dearmor
---
+++ mongo_scripts/mongo2.sh
@@ -0,0 +1,1 @@
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
